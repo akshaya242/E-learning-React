b2771ba40decbbd9c56ae01fdbb08fb4c9906dc1
Tanesh <tanesh753@gmail.com>
2024-11-19 01:05:31 +0530

    deleted akshaya

--- backend/routes/courses.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const courseController = require('../controllers/User');
-
-// Route to get filtered courses
-router.get('/filtered', courseController.getFilteredCourses);
-
-module.exports = router;
