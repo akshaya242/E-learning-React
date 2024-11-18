ba647f71ad90069671f7dbf1376bed35aaceb6c1
Akshayasree Padmanaban <akshayasree224@gmail.com>
2024-11-18 18:46:36 +0530

    sldier

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
+import { Link } from "react-router-dom";
 import "../style/Header.css";
-import logo from "../images/logo2.svg"
+import logo from "../images/logo2.svg";
 
 const Header = ({ user }) => {
   return (
-    <header>
-      <nav>
+    <header className="header">
+      <nav className="navbar">
         <div className="logo-container">
           <Link to="/">
             <img src={logo} alt="QuikLearn Logo" id="logo" />
           </Link>
         </div>
         <i className="fas fa-bars" id="ham-menu"></i>
-        <ul id="nav-bar">
+        <ul id="nav-bar" className="navbar-links">
           <li>
             <Link to="/">Home</Link>
           </li>
