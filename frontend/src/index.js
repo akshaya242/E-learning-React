debdd651f2bb99d49cfe1c0115407a87178910a5
Akshayasree Padmanaban <akshayasree224@gmail.com>
2024-12-02 12:16:01 +0530

    login and register func from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { userContextProvider } from './context/UserContext';
+import {UserContextProvider}  from './context/UserContext';
+
+export const server = "http://localhost:5000";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <userContextProvider><App /></userContextProvider>
+  <React.StrictMode>  
+   
+   <UserContextProvider>
+    <App />
+   </UserContextProvider>
     
   </React.StrictMode>
 );
