1f686f78bef846489360633a3e076eb477be11fd
Deraj Yojith <derajyojithr@gmail.com>
2024-12-02 20:47:45 +0530

    Razorpay Added

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllCourses, getSingleCourse ,fetchLectures,fetchlecutre, getMycourses} from '../controllers/course.js';
+import { getAllCourses, getSingleCourse ,fetchLectures,fetchlecutre, getMycourses, checkout, paymentVerification} from '../controllers/course.js';
 import {isAuth} from  '../middlewares/isAuth.js'
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.get("/course/:id",getSingleCourse);
 router.get('/lectures/:id',isAuth ,fetchLectures);
 router.get('/lecture/:id',isAuth ,fetchlecutre);
 router.get("/mycourse",isAuth,getMycourses)
+router.post("/course/checkout/:id",isAuth,checkout);
+router.post("/verification/:id",isAuth,paymentVerification);
 
 export default router; 
