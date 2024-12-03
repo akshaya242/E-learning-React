d148c7708cbca0956ec0e0169fb966f2d10bf5fe
Tanesh <tanesh753@gmail.com>
2024-12-03 20:56:48 +0530

    header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import "./Header.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
+import "./Header.css"; // Assuming you have the same CSS file
 
 const Header = ({isAuth}) => {
   return (
     <header>
-        <div className='logo'>E-Learning</div>
-
-        <div className='link'>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/courses"}>Courses</Link>
-            <Link to={"/about"}>About</Link>
-            {isAuth?
+      <nav>
+        <div className="logo-container">
+          <Link to="/">
+            <img src="images/logo2.svg" alt="QuikLearn Logo" id="logo" />
+          </Link>
+        </div>
+        <i className="fas fa-bars" id="ham-menu"></i>
+        <ul id="nav-bar">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/teachers">Teachers</Link>
+          </li>
+          <li>
+            <Link to="/faqs">FAQ</Link>
+          </li>
+          {isAuth?
             <Link to={"/account"}>Account</Link> :
               <Link to={"/login"}>Login </Link>}
-        </div>
-
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
