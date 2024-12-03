48d85ffcd6cbeb3e38cb3c7624287dd081e5202a
Akshayasree Padmanaban <akshayasree224@gmail.com>
2024-12-03 11:51:21 +0530

    admin dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, isAuth } from '../middlewares/isAuth.js';
-import { addLectures, createCourse, deleteCourse, deleteLecture, getAllstats } from '../controllers/admin.js';
+import { addLectures, createCourse, deleteCourse, deleteLecture, getAllstats, updateRole, getAllUser } from '../controllers/admin.js';
 import { uploadFiles } from '../middlewares/multer.js'
 const router = express.Router();
 
@@ -10,5 +10,7 @@ router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
 
 router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
 router.get("/stats",isAuth,isAdmin,getAllstats);
+router.put("/user/:id", isAuth, updateRole);
+router.get("/users", isAuth, isAdmin, getAllUser);
 
 export default router;
