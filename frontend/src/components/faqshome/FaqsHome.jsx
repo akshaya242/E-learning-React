5dc847233aba3dc5f3e80ac8f9004eb056f8d331
Akshayasree Padmanaban <akshayasree224@gmail.com>
2024-12-04 21:33:53 +0530

    unenroll

--- frontend/src/components/faqshome/FaqsHome.jsx
+++ frontend/src/components/faqshome/FaqsHome.jsx
@@ -18,27 +18,8 @@ const FaqsHome = () => {
             title: "Can I access course materials after completing the course?",
             content: `Yes, once you enroll in a course, you will have lifetime access to the course materials, even after you complete it.`,
           },
-          {
-            title: "What payment methods are accepted?",
-            content: `We accept various payment methods, including credit/debit cards and PayPal. Please check our payment page for more details.`,
-          },
-          {
-            title: "What is your refund policy?",
-            content: `If you are not satisfied with a course, you can request a refund within a specified period (e.g., 30 days) after enrollment, provided you have not completed a significant portion of the course.`,
-          },
-          {
-            title: "How can I contact customer support?",
-            content:
-              "You can reach our customer support team via email, live chat, or by submitting a support ticket through our website.",
-          },
-          {
-            title: "Can I interact with other learners?",
-            content: `Yes, our platform includes community features where you can engage with fellow learners, participate in discussions, and collaborate on projects.`,
-          },
-          {
-            title: "Do you offer mentorship or coaching?",
-            content: `Some courses may include mentorship or coaching options for an additional fee. Check the course details for availability.`,
-          },
+                  
+        
         ]}
   return (
     <div className="faq-sections">
